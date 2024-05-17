8c5e15f65f04c2d81a70680818618608dc35f587
alexapp02 <alexandrapuertabustamante@gmail.com>
2024-05-17 12:33:18 -0500

    Creación del repositorio

---
+++ juego.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+int main(){
+    cout<<"hola";
+    
+}
