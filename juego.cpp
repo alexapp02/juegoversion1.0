3bbe0b817225bc6ece9b9b88063b59e0fd86d688
Danielbest320 <Inaranjo@unal.edu.co>
2024-05-17 23:49:52 -0500

    Se agrega el proyecto

--- juego.cpp
+++ juego.cpp
@@ -4,10 +4,32 @@
 #include <vector>
 #include <algorithm>
 #include <cctype>
-using namespace std;
+#include <filesystem>
 
-int main(){
-    cout<<"hola";
-    cout<<"Soy Daniel";
-    
+using namespace std;
+namespace fs = std::filesystem;
+
+struct Datos {
+    string nombre;
+    int Id;
+};
+
+struct Jugador {
+    Datos datos;
+    int puntaje;
+    vector<vector<string>> pistas; // Vector de pistas para cada pregunta
+};
+
+Datos LeerDatos() {
+    Datos ingresarDato;
+
+    cout << "Ingrese su nombre: ";
+    getline(cin, ingresarDato.nombre);
+
+    cout << "Ingrese su ID: ";
+    cin >> ingresarDato.Id;
+    cin.ignore(); // Limpiar el buffer de entrada después de leer el ID
+
+    return ingresarDato;
 }
+
